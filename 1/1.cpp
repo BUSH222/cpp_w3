c64af7da43a3d9b3e8832124acb4aac4a7303569
Ted Vtorov <58144503+BUSH222@users.noreply.github.com>
2026-04-02 15:55:47 +0300

    cleaned this up

---
+++ 1/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "testing test\n";
+    cout << "testing test";
+    cout << "testing test bruh\n\r";
+    cout << 1+2;
+    cout << 12.445;
+    cout << -1.2;
+    return 0;
+}
---
+++ 1/2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "testing test" << "\n";
+    cout << "testing \ttestt" << endl;
+    return 0;
+}
